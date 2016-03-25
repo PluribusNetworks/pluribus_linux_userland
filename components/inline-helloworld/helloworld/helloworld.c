2b9558ff3d8f19943337a5d2cd3f5a5b51ba38bb
moinakg <moinakg@gmail.com>
2016-03-25 21:01:58 +0530

    Sample hello world program to demonstrate buildable source code maintained in this repo.

---
+++ components/inline-helloworld/helloworld/helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int
+main(void) {
+	printf("************* HELLO WORLD *************\n");
+	printf("* Pluribus Networks: Delivering the   *\n");
+	printf("* promise of SDN.                     *\n");
+	printf("***************************************\n");
+
+	return (0);
+}
+
